7ff4efe1d77198e83cf51ec7c51c78e5199f2b5f
nkalkstein <nkalkstein@gmail.com>
2018-04-09 00:09:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+
+
+const token = c6ecba581c152bde9ebf8d23a846c831181af7dc
+fetch('https://api.github.com/nkalkstein/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
+
+
+
 function getIssues() {
 }
 
